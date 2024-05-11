94120ec2110099f2d79a63c41ad030b2579a38f6
BA3000 <2864281096@qq.com>
2024-05-11 16:59:13 +0800

    update: add descriptions

--- singleton/src/lazySingletonTS.cpp
+++ singleton/src/lazySingletonTS.cpp
@@ -6,6 +6,8 @@ LazySingletonTS::LazySingletonTS() {
 }
 
 LazySingletonTS& LazySingletonTS::instance() {
+    // If multiple threads attempt to initialize the same static local variable concurrently, the initialization occurs exactly once (similar behavior can be obtained for arbitrary functions with
+    // std::call_once).
     static LazySingletonTS singleton;
     return singleton;
 }
--- singleton/src/lazySingletonTS.h
+++ singleton/src/lazySingletonTS.h
@@ -2,12 +2,13 @@
 
 class LazySingletonTS {
 public:
+    // alternatively you can return pointer instead
     static LazySingletonTS& instance();
 
     void Dosomething();
 
 private:
     LazySingletonTS();
-    LazySingletonTS(LazySingletonTS const&);
-    void operator=(LazySingletonTS const&);
+    LazySingletonTS( LazySingletonTS const& );
+    void operator=( LazySingletonTS const& );
 };
