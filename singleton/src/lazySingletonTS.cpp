b11c20772a36f356a494d503679283b02cc2bb61
BA3000 <2864281096@qq.com>
2024-05-13 10:54:52 +0800

    update: use inline function to create instance

--- singleton/src/lazySingletonTS.cpp
+++ singleton/src/lazySingletonTS.cpp
@@ -5,13 +5,6 @@ LazySingletonTS::LazySingletonTS() {
     std::cout << "lazySingletonTS ctor called" << std::endl;
 }
 
-LazySingletonTS& LazySingletonTS::instance() {
-    // If multiple threads attempt to initialize the same static local variable concurrently, the initialization occurs exactly once (similar behavior can be obtained for arbitrary functions with
-    // std::call_once).
-    static LazySingletonTS singleton;
-    return singleton;
-}
-
 void LazySingletonTS::Dosomething() {
     std::cout << "lazySingletonTS do something" << std::endl;
 }
--- singleton/src/lazySingletonTS.h
+++ singleton/src/lazySingletonTS.h
@@ -3,7 +3,12 @@
 class LazySingletonTS {
 public:
     // alternatively you can return pointer instead
-    static LazySingletonTS& instance();
+    static inline LazySingletonTS& instance() {
+        // If multiple threads attempt to initialize the same static local variable concurrently, the initialization occurs exactly once (similar behavior can be obtained for arbitrary functions with
+        // std::call_once).
+        static LazySingletonTS singleton;
+        return singleton;
+    }
 
     void Dosomething();
 
